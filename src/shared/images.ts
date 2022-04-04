de10b29949225f4d3ec550d0ad7c58bcc8b2252c
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-04 03:13:27 +0300

    chore: adding image imports

---
+++ src/shared/images.ts
@@ -0,0 +1,5 @@
+import landingMobile from '../assets/home/mobile/image-hero.jpg';
+import landingTablet from '../assets/home/tablet/image-hero.jpg';
+import landingDesktop from '../assets/home/desktop/image-hero.jpg';
+
+export const landing = [landingMobile, landingTablet, landingDesktop];
