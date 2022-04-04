122e3886878a086bfbb6a1350ddcb5a7471ba170
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-04 03:12:07 +0300

    style: adding base mixins

---
+++ src/shared/mixins.ts
@@ -0,0 +1,34 @@
+import { css } from 'styled-components';
+
+export const gridMixin = (columns: string, rows: string, gap?: string) => css`
+  display: grid;
+  grid-template-columns: ${columns};
+  grid-template-rows: ${rows};
+  gap: ${gap};
+`;
+
+export const flexMixin = (justify: string, align: string, direction: string) => css`
+  display: flex;
+  flex-direction: ${direction};
+  justify-content: ${justify};
+  align-items: ${align};
+`;
+
+export const maxContainer = css`
+  height: 100%;
+  width: 100%;
+`;
+
+export const resetDefaults = css`
+  border: none;
+  outline: none;
+  background: transparent;
+`;
+
+export const opacityMixin = css`
+  @supports ((-webkit-backdrop-filter: blur(15px)) or (backdrop-filter: blur(15px))) {
+    -webkit-backdrop-filter: blur(15px);
+    backdrop-filter: blur(15px) brightness(0.6) contrast(0.9);
+    background: rgba(255, 255, 255, 0.03);
+  }
+`;
