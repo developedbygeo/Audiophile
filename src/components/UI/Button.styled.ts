230f8a0e1274a3665be9e569d9857b5ae81a08a2
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-04 03:09:41 +0300

    style: adding the base buttons

---
+++ src/components/UI/Button.styled.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+import { resetDefaults } from '../../shared/mixins';
+import { BaseProps } from '../../shared/models/props.model';
+import { interactCta } from '../../shared/styles/interactive.styles';
+import { elevationThree } from '../../shared/styles/shadows.styles';
+
+export const UnstyledButton = styled.button<BaseProps>`
+  ${resetDefaults};
+  /* ${interactCta}; */
+  height: 5rem;
+  width: 5rem;
+  padding-block: 1rem;
+  padding-inline: 2rem;
+`;
+
+export const CtaButton = styled.button<BaseProps>`
+  ${resetDefaults}
+  width: 7rem;
+  height: 7em;
+  color: rgb(${({ theme }) => theme.colors.white});
+  background: rgb(${({ theme }) => theme.colors.brand});
+  font-weight: ${({ theme }) => theme.typography.weights.heading};
+  letter-spacing: 0.125rem;
+  ${interactCta}
+  ${elevationThree}
+`;
