6b228a57be3622a319c142faf19a0af5cea44d0c
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-04 03:54:16 +0300

    feat: adding the header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { GoThreeBars, GoX } from 'react-icons/go';
+import useWindow from '../../hooks/useWindow';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import StyledHeader from './Header.styled';
+import Nav from './Nav';
+import { UnstyledButton } from '../UI/Button.styled';
+
+const Header = () => {
+  const width = useWindow();
+  const { pathname } = useLocation();
+  const [navIsEnabled, setNavIsEnabled] = useState(false);
+  const navClass = width < 768 ? 'mobile-nav' : 'desktop-nav';
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => {
+      setNavIsEnabled(false);
+    }, 200);
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
+  const navToggleHandler = () => {
+    setNavIsEnabled((prevState) => !prevState);
+  };
+
+  const buttonContent = !navIsEnabled ? <GoThreeBars /> : <GoX />;
+
+  const shouldButtonRender = width < 768 && (
+    <UnstyledButton onClick={navToggleHandler} title="Toggle Menu" className="menu">
+      {buttonContent}
+    </UnstyledButton>
+  );
+
+  return (
+    <StyledHeader>
+      <Link to="/home" aria-label="Return to home">
+        <Logo className="logo" role="img" aria-label="logo" />
+      </Link>
+      {shouldButtonRender}
+      {width < 768 ? navIsEnabled && <Nav className={navClass} /> : <Nav className={navClass} />}
+    </StyledHeader>
+  );
+};
+
+export default Header;
