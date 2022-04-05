a84d643f2c5e0a3149290e31552f3d50671126e4
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-05 03:50:09 +0300

    chore: adding thumbnails for categories

--- src/shared/images.ts
+++ src/shared/images.ts
@@ -1,5 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import landingMobile from '../assets/home/mobile/image-hero.jpg';
 import landingTablet from '../assets/home/tablet/image-hero.jpg';
 import landingDesktop from '../assets/home/desktop/image-hero.jpg';
 
+import thumbnailEarphones from '../assets/shared/desktop/image-category-thumbnail-earphones.png';
+import thumbnailHeadphones from '../assets/shared/desktop/image-category-thumbnail-headphones.png';
+import thumbnailSpeakers from '../assets/shared/desktop/image-category-thumbnail-speakers.png';
+
 export const landing = [landingMobile, landingTablet, landingDesktop];
+
+export const thumbnails = [
+  { path: thumbnailEarphones, alt: 'Earphones', id: uuidv4() },
+  { path: thumbnailHeadphones, alt: 'Headphones', id: uuidv4() },
+  { path: thumbnailSpeakers, alt: 'Speakers', id: uuidv4() }
+];
