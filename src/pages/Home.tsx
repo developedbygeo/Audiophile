76dca07146dc05f035ebda9886c7a30951bc96a7
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-13 23:53:14 +0300

    feat: rendering featured

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Landing/Hero';
 import Categories from '../components/Landing/Categories';
+import Featured from '../components/Landing/Featured';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Categories />
+      <Featured />
     </>
   );
 };
