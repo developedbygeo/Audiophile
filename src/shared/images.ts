cf1d7c2ab36d3e0b17a94c44fbc4bf1790fcd6fd
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-13 23:52:50 +0300

    chore: adding more imports for home

--- src/shared/images.ts
+++ src/shared/images.ts
@@ -8,6 +8,18 @@ import thumbnailEarphones from '../assets/shared/desktop/image-category-thumbnai
 import thumbnailHeadphones from '../assets/shared/desktop/image-category-thumbnail-headphones.png';
 import thumbnailSpeakers from '../assets/shared/desktop/image-category-thumbnail-speakers.png';
 
+import zx9Mobile from '../assets/home/mobile/image-speaker-zx9.png';
+import zx9Tablet from '../assets/home/tablet/image-speaker-zx9.png';
+import zx9Desktop from '../assets/home/desktop/image-speaker-zx9.png';
+
+import zx7Mobile from '../assets/home/mobile/image-speaker-zx7.jpg';
+import zx7Tablet from '../assets/home/tablet/image-speaker-zx7.jpg';
+import zx7Desktop from '../assets/home/desktop/image-speaker-zx7.jpg';
+
+import yx1Mobile from '../assets/home/mobile/image-earphones-yx1.jpg';
+import yx1Tablet from '../assets/home/tablet/image-earphones-yx1.jpg';
+import yx1Desktop from '../assets/home/desktop/image-earphones-yx1.jpg';
+
 export const landing = [landingMobile, landingTablet, landingDesktop];
 
 export const thumbnails = [
@@ -15,3 +27,9 @@ export const thumbnails = [
   { path: thumbnailHeadphones, alt: 'Headphones', id: uuidv4() },
   { path: thumbnailSpeakers, alt: 'Speakers', id: uuidv4() }
 ];
+
+export const zx9 = [zx9Mobile, zx9Tablet, zx9Desktop];
+
+export const zx7 = [zx7Mobile, zx7Tablet, zx7Desktop];
+
+export const yx1 = [yx1Mobile, yx1Tablet, yx1Desktop];
