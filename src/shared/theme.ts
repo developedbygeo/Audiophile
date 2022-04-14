d4bd609a894f7af68e985c2a29bfade889dee707
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-14 03:37:34 +0300

    style: adding two more colors

--- src/shared/theme.ts
+++ src/shared/theme.ts
@@ -16,7 +16,9 @@ const myTheme: DefaultTheme = {
     brand: '216, 125, 74' || '#d87d4a',
     accent: '251, 175, 133' || '#fbaf85',
     black: '0, 0, 0' || '#000000',
-    smokeyBlack: '16, 16, 16' || '#101010'
+    smokeyBlack: '16, 16, 16' || '#101010',
+    davysGray: '76, 76, 76' || '#4c4c4c',
+    spanishGray: '151, 151, 151' || '#979797'
   }
 };
 
