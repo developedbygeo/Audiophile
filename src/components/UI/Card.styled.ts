0e1fd8ca8b7449590cb660c2955855bef6532ca2
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-14 03:36:09 +0300

    refactor: code cleanup

--- src/components/UI/Card.styled.ts
+++ src/components/UI/Card.styled.ts
@@ -11,17 +11,4 @@ export const Card = styled.div`
   a {
     color: rgb(${({ theme }) => theme.colors.black});
   }
-
-  & > .floating-img-wrapper {
-    position: absolute;
-    top: -50%;
-    left: 50%;
-    z-index: 10;
-    transform: translate(-50%, 0);
-    img {
-      width: 20rem;
-      height: 15rem;
-      object-fit: contain;
-    }
-  }
 `;
