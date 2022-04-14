074a0005c547306b7b6e10a94d523c02dcef4556
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-14 03:33:30 +0300

    feat: adding the brand section

---
+++ src/components/Brand/Brand.tsx
@@ -0,0 +1,24 @@
+import { StyledBrand, BrandImage, BrandText } from './Brand.styled';
+import { BigHeading, Description } from '../UI/Text.styled';
+
+const Brand = () => {
+  return (
+    <StyledBrand as="article">
+      <BrandImage className="image-cont" role="img" />
+      <BrandText>
+        <BigHeading>
+          BRINGING YOU THE<span> BEST</span> AUDIO GEAR
+        </BigHeading>
+        <Description>
+          Located at the heart of New York City, Audiophile is the premier store for high end headphones,
+          earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms
+          available for you to browse and experience a wide range of our products. Stop by our store to meet
+          some of the fantastic people who make Audiophile the best place to buy your portable audio
+          equipment.
+        </Description>
+      </BrandText>
+    </StyledBrand>
+  );
+};
+
+export default Brand;
