abd143479a03485a75e334ec6533a73d33718984
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-16 02:09:14 +0300

    feat: adding banner

--- src/pages/Earphones.tsx
+++ src/pages/Earphones.tsx
@@ -1,9 +1,11 @@
 import Products from 'components/Product/Products';
 import ScrollTop from 'components/UI/ScrollTop';
+import PageBanner from 'components/Header/PageBanner';
 
 const Earphones = () => (
   <>
     <ScrollTop />
+    <PageBanner page="earphones" />
     <Products category="earphones" />
   </>
 );
