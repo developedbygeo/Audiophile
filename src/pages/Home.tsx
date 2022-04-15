1f8dde7441dc1457b37bfc2ba6c4bc18ec041231
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-15 05:17:17 +0300

    chore: updating import statement

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Hero from 'components/Landing/Hero';
-import Categories from 'components/Landing/Categories';
+import Categories from 'components/Categories/Categories';
 import Featured from 'components/Landing/Featured';
 import Brand from 'components/Brand/Brand';
 
