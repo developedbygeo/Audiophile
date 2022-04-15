830b26a32202a8f7a1f979f43180b06373927b8a
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-16 02:10:17 +0300

    feat: rendering the new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import GlobalStyle from './shared/globalStyle';
-import Header from './components/Header/Header';
-import Home from './pages/Home';
-import Footer from './components/Footer/Footer';
+
+import Home from 'pages/Home';
+import Speakers from 'pages/Speakers';
+import Headphones from 'pages/Headphones';
+import Earphones from 'pages/Earphones';
+
+import GlobalStyle from 'shared/globalStyle';
+
+import Header from 'components/Header/Header';
+import Categories from 'components/Categories/Categories';
+import Brand from 'components/Brand/Brand';
+import Footer from 'components/Footer/Footer';
 
 function App() {
   return (
@@ -13,8 +21,14 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="home" element={<Home />} />
+          <Route path="speakers" element={<Speakers />} />
+          <Route path="headphones" element={<Headphones />} />
+          <Route path="earphones" element={<Earphones />} />
         </Routes>
+        <Categories />
+        <Brand />
       </main>
+
       <Footer />
     </>
   );
