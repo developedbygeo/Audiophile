a2a169655ae764d27b0726606296a6c15335e573
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-15 05:11:00 +0300

    feat: adding the mobile nav component

---
+++ src/components/Header/MobileNav.tsx
@@ -0,0 +1,12 @@
+import Categories from 'components/Categories/Categories';
+import { StyledMobileNav } from './Nav.styled';
+
+const MobileNav = () => {
+  return (
+    <StyledMobileNav>
+      <Categories />
+    </StyledMobileNav>
+  );
+};
+
+export default MobileNav;
