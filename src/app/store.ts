f2e6efd7d3f39eb5c4cf3570b925bbe1282b1385
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-15 05:18:19 +0300

    chore: adding the state slices

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import productReducer from '../features/productsSlice';
+import cartReducer from '../features/cartSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    products: productReducer,
+    cart: cartReducer
+  }
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
