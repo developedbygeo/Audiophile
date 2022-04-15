bfaea8a69c782a960728adb0dc10fa704560afbc
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-15 05:09:55 +0300

    chore: moving categories

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { IoChevronForwardSharp } from 'react-icons/io5';
+
+import { thumbnails } from 'shared/images';
+import { MediumHeading } from 'components/UI/Text.styled';
+
+import { StyledCategories, CategoryCard, ImageWrapper } from './Categories.styled';
+
+const Categories = () => {
+  return (
+    <StyledCategories>
+      {thumbnails.map(({ path, alt, id }) => (
+        <CategoryCard key={id}>
+          <ImageWrapper>
+            <img src={path} alt="sample category thumbnail" />
+          </ImageWrapper>
+          <div className="text-wrapper">
+            <MediumHeading as="h1">{alt}</MediumHeading>
+          </div>
+          <div>
+            <Link to={`/${alt.toLowerCase()}`} title={`Shop the latest ${alt.toLowerCase()}`}>
+              <span>SHOP</span>
+              <IoChevronForwardSharp />
+            </Link>
+          </div>
+        </CategoryCard>
+      ))}
+    </StyledCategories>
+  );
+};
+
+export default Categories;
