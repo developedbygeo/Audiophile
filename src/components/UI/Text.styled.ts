b6b0dbdabdb522736962911ea955c94d88499b12
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-16 02:08:33 +0300

    style: removing default color

--- src/components/UI/Text.styled.ts
+++ src/components/UI/Text.styled.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components/macro';
 
 export const Description = styled.p`
-  color: rgba(${({ theme }) => theme.colors.white}, 0.75);
   font-size: 1.8rem;
   font-weight: ${({ theme }) => theme.typography.weights.body};
   text-align: center;
@@ -11,7 +10,6 @@ export const Description = styled.p`
 `;
 
 export const BigHeading = styled.h1`
-  color: rgba(${({ theme }) => theme.colors.white}, 0.75);
   font-size: 4.5rem;
   padding-block: 1rem;
   text-transform: uppercase;
@@ -20,14 +18,12 @@ export const BigHeading = styled.h1`
 `;
 
 export const MediumHeading = styled.h2`
-  color: rgba(${({ theme }) => theme.colors.white}, 0.75);
   font-size: 3rem;
   text-transform: uppercase;
   margin-block: 1rem;
 `;
 
 export const SmallSubheading = styled.h4`
-  color: rgba(${({ theme }) => theme.colors.white}, 0.75);
   font-size: 2rem;
   text-transform: uppercase;
   font-weight: ${({ theme }) => theme.typography.weights.subheading};
