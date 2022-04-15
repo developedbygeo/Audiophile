45364e9fa7d9ca545f5e8918b48bf735a674658e
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-15 23:13:16 +0300

    feat: adding scroll to top component

---
+++ src/components/UI/ScrollTop.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 100);
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollTop;
