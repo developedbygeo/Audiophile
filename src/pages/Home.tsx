5380ff2e913ac2f5986916016b0304e9a8d7bb6f
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-18 03:17:25 +0300

    feat: adding featured

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import Hero from 'components/Landing/Hero';
 import ScrollTop from 'components/UI/ScrollTop';
+import Hero from 'components/Landing/Hero';
+import Featured from 'components/Landing/Featured';
 
 const Home = () => {
   return (
     <>
       <ScrollTop />
       <Hero />
+      <Featured />
     </>
   );
 };
