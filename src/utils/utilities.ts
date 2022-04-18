5c4fc856233762e88e036e96ec04fe9440f3cdfd
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-19 01:49:08 +0300

    chore: adding ordinal fn

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -3,3 +3,15 @@ export const derivedViewport = (width: number) => {
   if (width >= 768 && width < 1024) return 'tablet';
   return 'desktop';
 };
+
+export const ordinalLookup = {
+  '0': 'first',
+  '1': 'second',
+  '2': 'third'
+};
+
+export const getOrdinal = (n: number) => {
+  const formatted = n.toString();
+  if (formatted in ordinalLookup) return ordinalLookup[formatted as keyof typeof ordinalLookup];
+  return '';
+};
