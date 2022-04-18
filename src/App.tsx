f8034a68fd6ff69d923f239fb378d8b60d72467f
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-18 03:10:32 +0300

    feat: adding the product details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import GlobalStyle from 'shared/globalStyle';
+
 import Home from 'pages/Home';
 import Speakers from 'pages/Speakers';
 import Headphones from 'pages/Headphones';
 import Earphones from 'pages/Earphones';
-
-import GlobalStyle from 'shared/globalStyle';
+import Details from 'pages/Details';
 
 import Header from 'components/Header/Header';
 import Categories from 'components/Categories/Categories';
@@ -24,6 +25,7 @@ function App() {
           <Route path="speakers" element={<Speakers />} />
           <Route path="headphones" element={<Headphones />} />
           <Route path="earphones" element={<Earphones />} />
+          <Route path="products/:id" element={<Details />} />
         </Routes>
         <Categories />
         <Brand />
