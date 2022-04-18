a5ea0dca901b669d0c82892aa20b4ba2030a57c0
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-19 01:48:53 +0300

    chore: typing included and gallery

--- src/shared/models/product.model.ts
+++ src/shared/models/product.model.ts
@@ -3,3 +3,5 @@ import data from 'data.json';
 export type AllProductsType = typeof data;
 export type ProductType = typeof data[0];
 export type FeaturesType = typeof data[0]['features'];
+export type IncludedType = typeof data[0]['includes'];
+export type GalleryType = typeof data[0]['gallery'];
