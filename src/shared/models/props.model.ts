c5378b7b7a2083b0e4321c74797151531fcb8de1
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-21 02:55:24 +0300

    chore: typing modal props

--- src/shared/models/props.model.ts
+++ src/shared/models/props.model.ts
@@ -5,3 +5,21 @@ export type BaseProps = {
   onChange?: (id: number) => void;
   onBlur?: () => void;
 };
+
+export type LinkProps = BaseProps & {
+  look?: string;
+};
+
+export type ModalOverlayProps = {
+  children: React.ReactNode;
+};
+
+export type ModalBackdropProps = {
+  onDisable: () => void;
+};
+
+export type ModalDivProps = {
+  backdrop?: boolean;
+};
+
+export type ModalProps = ModalOverlayProps & ModalBackdropProps;
