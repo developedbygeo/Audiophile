9c9d171ff43fa71e899f81170bc525d581ea3744
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-21 02:55:03 +0300

    style: adding base modal styling

---
+++ src/components/UI/Modal.styled.ts
@@ -0,0 +1,44 @@
+import styled, { css } from 'styled-components';
+
+import { ModalDivProps } from 'shared/models/props.model';
+import { devices } from 'shared/breakpoints';
+import { maxContainer } from 'shared/mixins';
+
+const backdropStyle = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  ${maxContainer};
+  z-index: 20;
+  background-color: rgba(0, 0, 0, 0.65);
+`;
+
+const modalStyle = css`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 85%;
+  z-index: 30;
+  background-color: rgb(${({ theme }) => theme.colors.white});
+  transform: translate(-50%, -50%);
+  padding: 2rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+  border-radius: 1rem;
+  min-height: 25vh;
+  @media ${devices.laptop} {
+    width: 65vw;
+  }
+`;
+
+const getDivStyle = ({ backdrop }: ModalDivProps) => {
+  if (backdrop) {
+    return backdropStyle;
+  }
+  return modalStyle;
+};
+
+const DynamicDiv = styled.div`
+  ${getDivStyle}
+`;
+
+export default DynamicDiv;
