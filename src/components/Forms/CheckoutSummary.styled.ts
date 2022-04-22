40da5212dbc3b0ca7010e6dbbc405eb0ba89c717
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-23 02:10:37 +0300

    style: adding checkout summary styling

---
+++ src/components/Forms/CheckoutSummary.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+
+import { StyledCheckout } from './Checkout.styled';
+
+export const StyledSummary = styled(StyledCheckout)`
+  height: auto;
+  min-height: 30vh;
+`;
