af30f03874759d4263ac29141bfd5834648a50c6
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-23 02:11:49 +0300

    chore: code cleanup due to code consolidation

--- src/components/ModalDialogues/CartReview.styled.ts
+++ src/components/ModalDialogues/CartReview.styled.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components/macro';
 
 import { flexMixin } from 'shared/mixins';
-import { StyledImageCont } from 'components/Product/ProductPreview.styled';
 
 export const StyledReview = styled.div`
   min-height: 40vh;
@@ -44,60 +43,4 @@ export const CartList = styled.ul`
   overflow-y: auto;
   ${flexMixin('flex-start', 'center', 'column')};
   gap: 2rem;
-`;
-
-export const CartItem = styled.li`
-  width: 100%;
-  ${flexMixin('space-between', 'center', 'row')};
-`;
-
-export const CartImageCont = styled(StyledImageCont)`
-  flex: 0.3;
-  height: 100%;
-  width: 100%;
-  img {
-    height: 10vh;
-    width: 20vw;
-  }
-`;
-
-export const CartItemText = styled.div`
-  flex: 0.3;
-  gap: 1rem;
-  ${flexMixin('space-evenly', 'flex-start', 'column')};
-
-  p {
-    font-weight: ${({ theme }) => theme.typography.weights.heading};
-    color: rgba(${({ theme }) => theme.colors.black}, 0.6);
-  }
-`;
-
-export const CartItemQuantity = styled.div`
-  flex: 0.3;
-  ${flexMixin('space-evenly', 'center', 'row')};
-  background: rgb(${({ theme }) => theme.colors.silver});
-  border-radius: 0.25rem;
-
-  button {
-    font-weight: ${({ theme }) => theme.typography.weights.heading};
-    color: rgba(${({ theme }) => theme.colors.black}, 0.3);
-    @media (hover: hover) {
-      &:hover {
-        color: rgb(${({ theme }) => theme.colors.brand});
-      }
-    }
-  }
-`;
-
-export const CartPrice = styled.div`
-  width: 100%;
-  ${flexMixin('space-between', 'center', 'row')};
-  h2 {
-    font-size: 2rem;
-    font-weight: ${({ theme }) => theme.typography.weights.body};
-    color: rgba(${({ theme }) => theme.colors.black}, 0.6);
-  }
-  h3 {
-    font-size: 2.4rem;
-  }
 `;
