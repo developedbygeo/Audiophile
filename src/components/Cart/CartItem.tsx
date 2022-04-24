32f5d7b488c87f180ca7b97e88291ab7ae8de377
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-24 04:13:03 +0300

    refactor: modularizing component

--- src/components/Cart/CartItem.tsx
+++ src/components/Cart/CartItem.tsx
@@ -4,12 +4,22 @@ import { CartItem as ICartItem } from 'features/cartSlice';
 import { CartItemContainer, CartImageCont, CartItemText, CartItemQuantity } from './CartItem.styled';
 
 type ItemProps = {
+  className?: string;
   isCartView: boolean;
   onDecrement?: (id: number) => void;
   onIncrement?: (details: ICartItem) => void;
 } & ICartItem;
 
-const CartItem = ({ name, price, id, quantity, onDecrement, onIncrement, isCartView }: ItemProps) => {
+const CartItem = ({
+  name,
+  price,
+  id,
+  quantity,
+  onDecrement,
+  onIncrement,
+  isCartView,
+  className
+}: ItemProps) => {
   const productImage = getCartImage(name);
   const individualProductPrice = +price / +quantity;
   const detailsToDispatch = { name, price: individualProductPrice, id, quantity: 1 };
@@ -26,18 +36,20 @@ const CartItem = ({ name, price, id, quantity, onDecrement, onIncrement, isCartV
     </>
   );
 
-  const checkoutActions = <span>{quantity}</span>;
+  const checkoutActions = <span>x{quantity}</span>;
 
   return (
-    <CartItemContainer key={id}>
+    <CartItemContainer className={className || ''} key={id}>
       <CartImageCont>
         <img src={productImage} alt={name} />
       </CartImageCont>
-      <CartItemText>
+      <CartItemText className="item-text">
         <h4>{name}</h4>
         <p>$ {price.toLocaleString()}</p>
       </CartItemText>
-      <CartItemQuantity>{isCartView ? cartViewActions : checkoutActions}</CartItemQuantity>
+      <CartItemQuantity className={!isCartView ? 'checkout-quantity' : ''}>
+        {isCartView ? cartViewActions : checkoutActions}
+      </CartItemQuantity>
     </CartItemContainer>
   );
 };
