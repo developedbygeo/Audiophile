c9e04527d1112d902050e6d789ea248144e3ab1b
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-24 04:13:21 +0300

    style: updating styled checkout

--- src/components/Forms/Checkout.styled.ts
+++ src/components/Forms/Checkout.styled.ts
@@ -8,6 +8,20 @@ export const PageBackground = styled.div`
   width: 100%;
   height: auto;
   min-height: 100vh;
+  ${gridMixin('1fr', 'auto')};
+  gap: 4rem;
+`;
+
+export const StyledCheckout = styled(BigContainer)`
+  width: 90%;
+  height: auto;
+  min-height: 120vh;
+  padding-top: 0;
+  padding: 7.5% 5%;
+  margin-top: 6vh;
+  background: rgb(${({ theme }) => theme.colors.white});
+  ${gridMixin('1fr', 'auto 1fr')};
+  gap: 1rem;
 
   h1 {
     color: rgb(${({ theme }) => theme.colors.black});
@@ -19,18 +33,6 @@ export const PageBackground = styled.div`
   h2 {
     margin: 0;
   }
-`;
-
-export const StyledCheckout = styled(BigContainer)`
-  width: 90%;
-  height: auto;
-  min-height: 120vh;
-  padding-top: 0;
-  padding: 7.5% 5%;
-  margin-block: 7.5vh;
-  background: rgb(${({ theme }) => theme.colors.white});
-  ${gridMixin('1fr', 'auto 1fr')};
-  gap: 1rem;
 
   &::before {
     display: none;
@@ -42,16 +44,20 @@ export const StyledCheckout = styled(BigContainer)`
     letter-spacing: 0.1rem;
   }
 
-  form {
-    ${gridMixin('1fr', 'repeat(2, 1fr)')};
-    gap: 4rem;
+  .error {
+    width: 100%;
+    text-align: center;
+    color: rgb(${({ theme }) => theme.colors.ctaRed});
+    font-weight: ${({ theme }) => theme.typography.weights.heading};
+    letter-spacing: 0.12rem;
   }
 `;
 
 export const FormSection = styled.div`
   ${flexMixin('space-evenly', 'flex-start', 'column')};
-  min-height: 30vh;
+  min-height: 60vh;
   gap: 2rem;
+  padding-block: 3vh;
 
   label {
     font-weight: ${({ theme }) => theme.typography.weights.heading};
@@ -65,7 +71,7 @@ export const FormSection = styled.div`
 `;
 
 export const InputContainer = styled.div`
-  ${flexMixin('space-between', 'flex-start', 'column-reverse')};
+  ${flexMixin('space-between', 'flex-start', 'column')};
   gap: 1.5rem;
   width: 100%;
 `;
