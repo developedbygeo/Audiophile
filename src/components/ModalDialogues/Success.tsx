43fcd7eb83ab4d9ec3822625dbe3536c230e6e6a
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-26 03:15:35 +0300

    feat: adding success item summary

--- src/components/ModalDialogues/Success.tsx
+++ src/components/ModalDialogues/Success.tsx
@@ -1,18 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { ModalBackdropProps as DialogueProps } from 'shared/models/props.model';
 
 // TODO finish component;
 
-import { useAppSelector } from 'app/hooks';
+import { useAppSelector, useAppDispatch } from 'app/hooks';
+import { resetCart } from 'features/cartSlice';
+import useWindowWidth from 'hooks/useWindow';
+import { getSpecificItem, derivedViewport, getImagePath } from 'utils/utilities';
+
+import { ModalBackdropProps as DialogueProps } from 'shared/models/props.model';
 
 import { BsCheckLg } from 'react-icons/bs';
 import { BigHeading, Description } from 'components/UI/Text.styled';
+import { StyledImageCont } from 'components/Product/ProductPreview.styled';
 
 import { StyledReview as StyledContainer } from './CartReview.styled';
-import { Checkmark, SuccessText } from './Success.styled';
+import {
+  Checkmark,
+  SuccessText,
+  SuccessItem,
+  SuccessItemText,
+  SuccessQuantity,
+  SuccessSummary
+} from './Success.styled';
 
-const Success = () => {
-  const { totalCost, shipping } = useAppSelector((state) => state.cart);
+const Success = ({ onDisable }: DialogueProps) => {
+  const { products, totalCost, shipping } = useAppSelector((state) => state.cart);
+  const allProducts = useAppSelector((state) => state.products);
+  const topProductInCart = products[0];
+  const productData = getSpecificItem(allProducts, topProductInCart.id);
+  const dispatch = useAppDispatch();
+  const width = useWindowWidth();
+  const viewport = derivedViewport(width);
+  const productImage = getImagePath(productData.categoryImage[viewport]);
+
+  const resetCartHandler = () => {
+    dispatch(resetCart());
+    if (onDisable) onDisable();
+  };
 
   return (
     <StyledContainer as="section">
@@ -23,7 +47,23 @@ const Success = () => {
         <BigHeading as="h1">Thank you for your order</BigHeading>
         <Description>You will receive an email confirmation shortly.</Description>
       </SuccessText>
-      <button type="button">Hi</button>
+      <SuccessSummary>
+        <SuccessItem>
+          <StyledImageCont>
+            <img src={productImage} alt={topProductInCart.name} />
+          </StyledImageCont>
+          <SuccessItemText>
+            <BigHeading as="h4">{topProductInCart.name}</BigHeading>
+            <Description>$ {topProductInCart.price.toLocaleString()}</Description>
+          </SuccessItemText>
+          <SuccessQuantity>
+            <Description>x{topProductInCart.quantity}</Description>
+          </SuccessQuantity>
+        </SuccessItem>
+      </SuccessSummary>
+      <button onClick={resetCartHandler} type="button">
+        Hi
+      </button>
     </StyledContainer>
   );
 };
