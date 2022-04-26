2b4849a0341fb07311bf12f3f83a42c86ecd63eb
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-26 03:15:51 +0300

    style: adding success styling

--- src/components/ModalDialogues/Success.styled.ts
+++ src/components/ModalDialogues/Success.styled.ts
@@ -1,5 +1,7 @@
-import { flexMixin } from 'shared/mixins';
 import styled from 'styled-components/macro';
+
+import { flexMixin } from 'shared/mixins';
+import { CartItemText } from 'components/Cart/CartItem.styled';
 
 export const Checkmark = styled.div`
   height: 10vh;
@@ -25,5 +27,59 @@ export const SuccessText = styled.article`
   h1,
   p {
     text-align: left;
+  }
+`;
+
+export const SuccessSummary = styled.article`
+  ${flexMixin('center', 'center', 'column')};
+  padding: 1rem;
+  .price {
+    background: rgb(${({ theme }) => theme.colors.black});
+    flex: 0.5;
+  }
+`;
+
+export const SuccessQuantity = styled.div`
+  ${flexMixin('center', 'center', 'column')};
+
+  p {
+    color: rgba(${({ theme }) => theme.colors.black}, 0.6);
+    font-weight: ${({ theme }) => theme.typography.weights.heading};
+  }
+`;
+
+export const SuccessItem = styled.div`
+  position: relative;
+  padding: 5rem 2rem;
+  flex: 1;
+  ${flexMixin('space-between', 'center', 'row')};
+  background: rgb(${({ theme }) => theme.colors.silver});
+
+  & > div {
+    ${flexMixin('center', 'center', 'column')};
+    flex: 0.2;
+  }
+
+  &::before {
+    position: absolute;
+    content: '';
+    bottom: 2rem;
+    right: 50%;
+    transform: translate(50%, 0%);
+    height: 0.2rem;
+    width: 90%;
+    background: rgba(${({ theme }) => theme.colors.black}, 0.15);
+  }
+`;
+
+export const SuccessItemText = styled(CartItemText)`
+  h4 {
+    font-size: 2rem;
+    margin: 0;
+  }
+  h4,
+  p {
+    text-align: left;
+    align-self: flex-start;
   }
 `;
