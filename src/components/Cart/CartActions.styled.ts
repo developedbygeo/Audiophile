89cb1d50e64d0bb52f6608fcbac5fa48ea3823d0
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-29 00:59:41 +0300

    chore: adding gray description

--- src/components/Cart/CartActions.styled.ts
+++ src/components/Cart/CartActions.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/macro';
 
 import { flexMixin } from 'shared/mixins';
+import { grayDescription } from 'components/UI/Text.styled';
 
 export const CartPrice = styled.div`
   width: 100%;
@@ -8,7 +9,7 @@ export const CartPrice = styled.div`
   h2 {
     font-size: 2rem;
     font-weight: ${({ theme }) => theme.typography.weights.body};
-    color: rgba(${({ theme }) => theme.colors.black}, 0.6);
+    ${grayDescription};
   }
   h3 {
     font-size: 2.4rem;
