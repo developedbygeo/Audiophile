cc6b86454334cc04b5d5b47d46c36ce9ca5e447a
georgiosan <georgiosanagnostou@hotmail.com>
2022-04-30 00:26:15 +0300

    fix: rendering categories in the correct order

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import ScrollTop from 'components/UI/ScrollTop';
 import Hero from 'components/Landing/Hero';
 import Featured from 'components/Landing/Featured';
+import Categories from 'components/Categories/Categories';
 
 const Home = () => {
   return (
     <>
       <ScrollTop />
       <Hero />
+      <Categories />
       <Featured />
     </>
   );
