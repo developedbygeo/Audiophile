19e8d1eee0110af4fec7af1b4e66cecf0ba40ff0
georgiosan <georgiosanagnostou@hotmail.com>
2022-05-01 01:30:25 +0300

    refactor: setting viewport-based classname

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -1,10 +1,15 @@
+import useWindowWidth from 'hooks/useWindow';
+import { derivedViewport } from 'utils/utilities';
+
 import Categories from 'components/Categories/Categories';
 import { StyledMobileNav } from './Nav.styled';
 
 const MobileNav = () => {
+  const width = useWindowWidth();
+  const viewport = derivedViewport(width);
   return (
     <StyledMobileNav>
-      <Categories />
+      <Categories className={`${viewport}-nav`} />
     </StyledMobileNav>
   );
 };
