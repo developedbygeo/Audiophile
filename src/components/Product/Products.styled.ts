18b582c6d15cf1589ba0c9e13f83e42a8b6f07d2
georgiosan <georgiosanagnostou@hotmail.com>
2022-05-01 01:32:02 +0300

    style: adjusting margin for tablets

--- src/components/Product/Products.styled.ts
+++ src/components/Product/Products.styled.ts
@@ -1,9 +1,15 @@
 import styled from 'styled-components/macro';
 
+import { devices } from 'shared/breakpoints';
 import { gridMixin } from 'shared/mixins';
 
 export const StyledProducts = styled.section`
   ${gridMixin('1fr', 'auto')};
   padding: 0;
   gap: 10vh;
+  margin: 2vh 0 10vh 0;
+
+  @media ${devices.tablet} {
+    margin-block: 10vh;
+  }
 `;
