7d9aaf093a5391279779ba0497a5cb5be642d187
georgiosan <georgiosanagnostou@hotmail.com>
2022-05-02 00:02:31 +0300

    style: adjusting for tablet viewports

--- src/components/Product/ProductGallery.styled.ts
+++ src/components/Product/ProductGallery.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/macro';
 
-import { flexMixin } from 'shared/mixins';
+import { devices } from 'shared/breakpoints';
+import { flexMixin, gridMixin } from 'shared/mixins';
 
 export const StyledGallery = styled.section`
   height: auto;
@@ -9,9 +10,23 @@ export const StyledGallery = styled.section`
   ${flexMixin('flex-start', 'center', 'column')};
   gap: 1vh;
 
+  @media ${devices.tablet} {
+    ${gridMixin('0.5fr 1fr', 'auto')};
+    min-height: initial;
+    height: auto;
+
+    .cont-3 {
+      grid-area: 1/2/3/2;
+    }
+  }
+
   & > div:nth-child(3) {
     img {
       height: 50vh;
+      @media ${devices.tablet} {
+        height: 100%;
+        object-fit: contain;
+      }
     }
   }
 `;
@@ -20,6 +35,11 @@ export const ImageContainer = styled.div`
   height: auto;
   width: 100%;
   margin: auto;
+
+  @media ${devices.tablet} {
+    height: 100%;
+    object-fit: contain;
+  }
 `;
 
 export const GalleryImage = styled.img`
@@ -27,4 +47,8 @@ export const GalleryImage = styled.img`
   height: 30vh;
   object-fit: cover;
   border-radius: 1rem;
+
+  @media ${devices.tablet} {
+    height: 100%;
+  }
 `;
