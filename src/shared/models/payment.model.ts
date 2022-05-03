53ca6e481db243688205c2e12f175c76cbff3d46
georgiosan <georgiosanagnostou@hotmail.com>
2022-05-04 00:18:11 +0300

    chore: typing payment data

--- src/shared/models/payment.model.ts
+++ src/shared/models/payment.model.ts
@@ -1,6 +1,6 @@
 import { paymentData } from 'utils/checkoutData';
 
 export type DataKeys = keyof typeof paymentData;
+export type PaymentDataType = typeof paymentData.paymentDetails;
 export type PaymentFieldType = keyof typeof paymentData.paymentMethods[0];
 export type MethodTypes = 'emoney' | 'cod';
-export type PaymentInputType = typeof paymentData.paymentDetails;
