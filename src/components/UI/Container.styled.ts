c9352e55bfc67b32f39668737f176c150188a68e
georgiosan <georgiosanagnostou@hotmail.com>
2022-05-10 01:07:21 +0300

    style: adding 404 page's layout and mq

--- src/components/UI/Container.styled.ts
+++ src/components/UI/Container.styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components/macro';
 
 import placeholderImage from 'assets/shared/404-cover.jpg';
 import { flexMixin } from 'shared/mixins';
+import { devices } from 'shared/breakpoints';
 
 export const Container = styled.div`
   height: 100%;
@@ -17,37 +18,70 @@ export const LargeSimpleContainer = styled(Container)`
 `;
 
 export const NotFoundLayout = styled.section`
-  ${flexMixin('space-around', 'flex-end', 'column')};
+  ${flexMixin('space-around', 'flex-end', 'row')};
+  background: url(${placeholderImage}) top no-repeat;
+  background-size: cover;
   padding: 1rem;
   padding-top: 5vh;
+  margin-bottom: 10vh;
   position: relative;
-  h1 {
-    width: 35vw;
-    margin: 0;
-    font-size: 3rem;
-    text-align: left;
-    text-transform: initial;
-    z-index: 10;
+
+  & > article {
+    width: 100%;
+    height: auto;
+    ${flexMixin('space-evenly', 'center', 'column')};
+    min-height: 40vh;
+    padding: 2vw;
+    gap: 1vh;
+    color: rgb(${({ theme }) => theme.colors.white});
+    background: rgba(${({ theme }) => theme.colors.smokeyBlack}, 0.55);
+    border-radius: 2rem;
+    h1 {
+      margin: 0;
+      text-transform: initial;
+      font-size: 3.5rem;
+    }
+    p {
+      font-size: 2rem;
+      padding-block: 1rem;
+      color: rgb(${({ theme }) => theme.colors.white});
+    }
+    @supports ((-webkit-backdrop-filter: blur(5px)) or (backdrop-filter: blur(5px))) {
+      backdrop-filter: blur(7.5px);
+      background: rgba(${({ theme }) => theme.colors.smokeyBlack}, 0.1);
+    }
   }
 
-  .placeholder {
-    position: absolute;
-    top: 0;
-    right: 25vw;
-    width: 100vw;
-    height: 100vh;
+  @media ${devices.tablet} {
+    align-items: center;
+    & > article {
+      background: rgba(${({ theme }) => theme.colors.smokeyBlack}, 0.75);
+    }
+  }
+
+  @media ${devices.laptop} {
+    flex-direction: column;
+    align-items: flex-end;
     background: url(${placeholderImage}) center no-repeat;
     background-size: cover;
-    &::before {
-      position: absolute;
-      content: '';
-      right: -20vh;
-      bottom: -20vh;
-      width: 30%;
-      height: 120vh;
-      background: white;
-      transform: rotate(25deg);
-      pointer-events: none;
+
+    & > article {
+      ${flexMixin('space-evenly', 'center', 'column')};
+      background: rgba(${({ theme }) => theme.colors.smokeyBlack}, 0.45);
+      width: 35%;
+      margin-right: 2vw;
+    }
+  }
+  @media ${devices.landscapeMobileSS} {
+    justify-content: flex-end;
+    background: url(${placeholderImage}) center no-repeat;
+    background-size: cover;
+
+    & > article {
+      ${flexMixin('space-evenly', 'center', 'column')};
+      background: rgba(${({ theme }) => theme.colors.smokeyBlack}, 0.45);
+      width: 35%;
+      margin-right: 2vw;
     }
   }
 `;
