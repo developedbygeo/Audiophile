9178c016e84ce353160c9f20d6ab88ba82d24e77
georgiosan <georgiosanagnostou@hotmail.com>
2022-05-10 01:07:59 +0300

    refactor: code cleanup with a mixin

--- src/shared/globalStyle.ts
+++ src/shared/globalStyle.ts
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
-import { devices } from './breakpoints';
-import { gridMixin } from './mixins';
+// import { devices } from './breakpoints';
+import { gridMixin, responsiveBaseFont } from './mixins';
 
 const GlobalStyle = createGlobalStyle`
+
 *{
     margin: 0;
     padding: 0;
@@ -11,48 +12,7 @@ const GlobalStyle = createGlobalStyle`
 
 html{
     font-size: 62.5%;
-    @media ${devices.mobileSS}{
-      font-size: 27%;
-    }
-    @media ${devices.mobileS}{
-      font-size: 40%;
-    }
-    @media ${devices.mobileM}{
-      font-size: 47.5%;
-    }
-    @media ${devices.mobileL}{
-      font-size: 55%;
-    }
-    @media ${devices.tablet}{
-      font-size: 65%;
-    }
-    @media ${devices.laptopM}{
-      font-size: 58.5%;
-    }
-    @media ${devices.laptopL}{
-      font-size: 60.5%;
-    }
-    @media ${devices.laptopXL}{
-      font-size: 65%;
-    }
-    @media ${devices.desktop}{
-      font-size: 100%;
-    }
-    @media ${devices.landscapeMobileSS}{
-      font-size: 22%;
-    }
-    @media ${devices.landscapeMobileS}{
-      font-size: 32.5%;
-    }
-    @media ${devices.landscapeMobileM}{
-      font-size: 40%;
-    }
-    @media ${devices.landscapeMobileL}{
-      font-size: 45%;
-    }
-    @media ${devices.desktop4K}{
-      font-size: 135%;
-    }
+    ${responsiveBaseFont};
 }
 body{
     height: auto;
